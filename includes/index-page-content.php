6adf20d8870d743e5533bd1184480e8bfd42b861
Gwyn Jones <m@gwnjns.com>
2015-10-01 16:29:27 +0100

    Changing H2 to H1.

--- includes/index-page-content.php
+++ includes/index-page-content.php
@@ -1,7 +1,7 @@
 <section class="row">
     <div class="col starts-at-full clr holding-box">
         <div class="heading-holding-banner">
-            <h2><span><span>[Page title]</span></span></h2>
+            <h1><span><span>[Page title]</span></span></h1>
         </div>
         <div class="breather">
             <p>[Page content]</p>
